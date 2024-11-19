accc5b294b8b2baca572210217009d7ea49909a2
edwardbhm <edu98.com@gmail.com>
2024-11-19 12:26:42 +0100

    Final

---
+++ app/src/main/java/com/example/juegodeprobabilidad2/IntroActivity.java
@@ -0,0 +1,27 @@
+package com.example.juegodeprobabilidad2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button btnStartGame = findViewById(R.id.vms);
+
+        btnStartGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
